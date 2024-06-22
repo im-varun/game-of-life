e36cb90868f5f3c0dca802327dfcfb1223cab305
im-varun <vrun.mulchandani@gmail.com>
2024-06-22 17:32:51 -0500

    Add constants and basic setup

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+
+WIDTH, HEIGHT = 800, 800
+TILE_SIZE = 20
+
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
+
+FPS = 120
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+clock = pygame.time.Clock()
