d5d00dc5dd8308ff67d279becf2a69784c273e7e
im-varun <vrun.mulchandani@gmail.com>
2024-06-22 18:54:57 -0500

    Add basic functionality to draw cells on grid

--- game_of_life.py
+++ game_of_life.py
@@ -4,6 +4,7 @@ pygame.init()
 
 BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
+WHITE = (255, 255, 255)
 
 WIDTH, HEIGHT = 800, 800
 TILE_SIZE = 20
@@ -18,11 +19,17 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 def draw_grid(positions):
-    for row in range(GRID_HEIGHT):
-        pygame.draw.line(screen, GREY, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
+    for position in positions:
+        column, row = position
+        top_left = (column * TILE_SIZE, row * TILE_SIZE)
 
-    for column in range(GRID_WIDTH):
-        pygame.draw.line(screen, GREY, (column * TILE_SIZE, 0), (column * TILE_SIZE, HEIGHT))
+        pygame.draw.rect(screen, WHITE, (*top_left, TILE_SIZE, TILE_SIZE))
+
+    for x in range(GRID_WIDTH):
+        pygame.draw.line(screen, GREY, (x * TILE_SIZE, 0), (x * TILE_SIZE, HEIGHT))
+
+    for y in range(GRID_HEIGHT):
+        pygame.draw.line(screen, GREY, (0, y * TILE_SIZE), (WIDTH, y * TILE_SIZE))
 
 def main_loop():
     running = True
