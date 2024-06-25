b86707f40a52e5902743575f952a71f07deba774
im-varun <vrun.mulchandani@gmail.com>
2024-06-25 16:42:05 -0500

    Add key press to quit game with escape key

--- game_of_life.py
+++ game_of_life.py
@@ -32,7 +32,7 @@ def gameoflife():
 
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if (event.type == pygame.QUIT) or ((event.type == pygame.KEYDOWN) and (event.key == pygame.K_ESCAPE)):
                 pygame.quit()
                 return
             elif event.type == pygame.KEYDOWN:
